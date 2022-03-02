c8f8adaa0759d92a0ea671d8be8cfebd7fe7ac81
Hemachandra S <97468545+HemachandraS@users.noreply.github.com>
2022-03-02 23:32:33 +0530

    Built a simple calculator using only node.js

---
+++ calculator/main.js
@@ -0,0 +1,10 @@
+const add = require('./add');
+const sub = require('./subtract');
+const mod = require('./multiply');
+const div=require('./divide');
+
+
+console.log(add(2,8));
+console.log(sub(2,8));
+console.log(mod(2,8));
+console.log(div(2,8));
